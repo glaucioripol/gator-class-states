ea171e58d012eb374aaf322800c3f8b18d384150
gjunior <glaucioripol@gmail.com>
2021-11-23 08:23:44 -0300

    chore(setup): first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gator class
+
+## Iremos exemplificar os estados citados na talk
+
+- [ ] estado transacional;
+- [ ] estado de UI;
+- [ ] estados client;
+- [ ] estados persistentes;
+- [ ] estado de comunicação;
---
+++ src/pages/transactional-state/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function TransactionalIndex() {
+  return (
+    <div>
+      <Head>
+        <title>Estado transacional</title>
+      </Head>
+
+    </div>
+  )
+}
